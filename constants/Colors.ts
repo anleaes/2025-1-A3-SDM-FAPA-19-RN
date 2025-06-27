727d77b04a0452b648a8ae5b19e87f664092e8fd
franciscosiq <franciscosiq@gmail.com>
2025-06-27 05:18:39 -0300

    adicionando constants de estilização

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,26 +1,15 @@
-/**
- * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
- * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
- */
-
-const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
-
 export const Colors = {
-  light: {
-    text: '#11181C',
-    background: '#fff',
-    tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
-    tabIconSelected: tintColorLight,
-  },
-  dark: {
-    text: '#ECEDEE',
-    background: '#151718',
-    tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
-    tabIconSelected: tintColorDark,
-  },
+    primary: '#607D8B',
+    accent: '#455A64',
+    background: '#ECEFF1',
+    text: '#212121',
+    lightText: '#757575',
+    white: '#FFFFFF',
+    red: '#B71C1C',
+    green: '#2E7D32',
+    danger: '#E54848',
+    border: '#B0BEC5',
+    cardBackground: '#CFD8DC',
+    activityIndicator: '#607D8B',
+    lightGray: '#F0F0F0',
 };
